ea296cad7b0b0b0c3f20c51a003692851c4de561
anu071295 <nanuredha0795@gmail.com>
2020-09-13 15:11:08 -0400

    navigate withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,7 +14,7 @@ class Directory extends React.Component{
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -48,13 +48,20 @@ class Directory extends React.Component{
     }
     render(){
         return(
+            
             <div className = 'directory-menu'>
-                {this.state.sections.map(({title,imageUrl,size,id}) => (
-                    <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size}/>
-                ))}
-            </div>
+            {this.state.sections.map(({id, ...otherSectionProps}) => (
+                <MenuItem key = {id} {...otherSectionProps}/>
+            ))}
+        </div>
         );
     }
 }
+
+//<div className = 'directory-menu'>
+                //{this.state.sections.map(({title,imageUrl,size,id}) => (
+                  //  <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size}/>
+               // ))}
+            //</div>
 
 export default Directory;
