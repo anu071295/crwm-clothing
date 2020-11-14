37b790314853b6181cd30c07d12f7cc3dc9e6f0c
anu071295 <nanuredha0795@gmail.com>
2020-11-14 12:45:43 -0500

    Updates

---
+++ precache-manifest.13930e279213e8e114675aafc8cba8e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d46b6b1f6b0226cd71ec6f0929bdc1",
+    "url": "/crwm-clothing/index.html"
+  },
+  {
+    "revision": "9cfa394a9351f9fa013f",
+    "url": "/crwm-clothing/static/css/main.714b1426.chunk.css"
+  },
+  {
+    "revision": "0edddab151911c0ee37b",
+    "url": "/crwm-clothing/static/js/2.a14cd375.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/crwm-clothing/static/js/2.a14cd375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfa394a9351f9fa013f",
+    "url": "/crwm-clothing/static/js/main.965d5405.chunk.js"
+  },
+  {
+    "revision": "d9fdd8bf3dfeae201816",
+    "url": "/crwm-clothing/static/js/runtime-main.33ae79ff.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwm-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
